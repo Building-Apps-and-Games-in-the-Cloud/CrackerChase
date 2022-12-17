f54be507cd5dd5ef1383b52b31903c8405073eb2
Rob Miles <rob@robmiles.com>
2022-12-17 21:36:10 +0000

    Bouncing cheese

---
+++ crackerchase.mjs
@@ -0,0 +1,23 @@
+import express from 'express';
+
+import {index} from './routes/index.mjs';
+
+// Create the express application
+const app = express();
+
+// Select the middleware to decode incoming posts
+app.use(express.urlencoded({ extended: false }));
+
+// Select ejs middleware
+app.set('view-engine', 'ejs');
+
+app.use (express.static('public'));
+
+
+// Connect the route handlers to the routes
+//app.use('/index.html', index);
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log("Server running");
+})
